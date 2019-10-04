4bd54daa2fa1057d226daab4450d6f6ad01b5e30
Tero Viitala <vite19ax@student.ju.se>
2019-10-04 20:03:24 +0200

    Issue: Lägg till kommentarer, förbättra funktionalitet och snygga till ver. 4

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-
+// Får åtkomst till element i HTML-filen:
 let bodye = document.getElementById('body-1');
 let ulLander = document.getElementById('ul-Lander');
 let navb = document.getElementById('nav1');
 
+// Hämtar JSON data för länder:
 fetch('land.json')
     .then(function(response) {
         return response.json();
     })
     .then(function(lander) {
 
+        // Lagrar JOSN data för länder i en array:
         let arrayLander = [];
         for (a=0; a<lander.length; a++) {
             arrayLander.push(lander[a].id);
         }
-
+        // Lagrar länder i en lista för meny:
         liLander = "";
         for (i=0; i<lander.length; i++) {
-            liLander += '<li id=\"li-' + lander[i].id + '\"> ' + lander[i].countryname + ' (Click) </li>';
+            liLander += '<li id=\"li-' + lander[i].id + '\"> ' + lander[i].countryname + ' (TRYCK HÄR FÖR ATT SE LISTA) </li>';
         }
         ulLander.innerHTML = liLander;
 
@@ -24,10 +26,12 @@ fetch('land.json')
     })
     .then(function(arrayLander) {
 
+        // Får åtkomst till de skapade elementen med länder:
         li11 = document.getElementById('li-1');
         li22 = document.getElementById('li-2');
         li33 = document.getElementById('li-3');
 
+        // Hämtar data för städer:
         fetch('stad.json')
         .then(function(response) {
             return response.json();
@@ -39,8 +43,10 @@ fetch('land.json')
                 return b.population - a.population;
             });
 
+            // Skapar en click funktion för första elementet som innehåller länder:
             li11.addEventListener('click', function() {
 
+                // Skapar en undermeny med lista att lagra städer i:
                 ul111 = document.createElement('ul');
                 navb.appendChild(ul111);
 
@@ -56,8 +62,10 @@ fetch('land.json')
                 } // Slut på loop
             }); // Slut på eventlistener
 
+            // Skapar en click funktion för andra elementet som innehåller länder:
             li22.addEventListener('click', function() {
 
+                // Skapar en undermeny med lista att lagra städer i:
                 ul222 = document.createElement('ul');
                 navb.appendChild(ul222);
 
@@ -73,8 +81,10 @@ fetch('land.json')
                 } // Slut på loop
             }); // Slut på eventlistener
 
+            // Skapar en click funktion för tredje elementet som innehåller länder:
             li33.addEventListener('click', function() {
 
+                // Skapar en undermeny med lista att lagra städer i:
                 ul333 = document.createElement('ul');
                 navb.appendChild(ul333);
 
@@ -91,18 +101,22 @@ fetch('land.json')
             }); // Slut på eventlistener
         })
     })
+    // Catch funktion som fångar upp fel om data inte hämtas korrekt:
     .catch(function(err) {
         console.log(JSON.stringify(err));
     });
 
+    // Funktion som hanterar click för elementen som innehåller städer:
     function display(x) {
 
+        // Hämtar data för länder:
         fetch('land.json') 
             .then(function(response) {
                 return response.json();
             })
             .then(function(lander2) {
 
+                // Lagrar data för länder i en array:
                 let arrayLandid = [];
                 for (c=0; c<lander2.length; c++) {
                     arrayLandid.push(lander2[c].id);
@@ -112,6 +126,7 @@ fetch('land.json')
             })
             .then(function(arrayLandid) {
 
+                // Hämtar data för städer:
                 fetch('stad.json')
                     .then(function(response) {
                     return response.json();
@@ -123,6 +138,7 @@ fetch('land.json')
                     return b.population - a.population;
                     });
 
+                    // Lagrar fakta om städerna tillhörande första landet i paragrafer i HTML-filen:
                     if (stader[x].countryid === arrayLandid[0]) {
                     document.getElementById('fakta').innerHTML = '' + stader[x].stadname + ' är en stad i ';
                     document.getElementById('inv').innerHTML = stader[x].population;
@@ -138,6 +154,7 @@ fetch('land.json')
                         })
                     } // Slut på if
 
+                    // Lagrar fakta om städerna tillhörande andra landet i paragrafer i HTML-filen:
                     if (stader[x].countryid === arrayLandid[1]) {
                         document.getElementById('fakta').innerHTML = '' + stader[x].stadname + ' är en stad i ';
                         document.getElementById('inv').innerHTML = stader[x].population;
@@ -153,6 +170,7 @@ fetch('land.json')
                         })
                     } // Slut på if
 
+                    // Lagrar fakta om städerna tillhörande tredje landet i paragrafer i HTML-filen:
                     if (stader[x].countryid === arrayLandid[2]) {
                         document.getElementById('fakta').innerHTML = '' + stader[x].stadname + ' är en stad i ';
                         document.getElementById('inv').innerHTML = stader[x].population;
@@ -169,25 +187,33 @@ fetch('land.json')
                     } // Slut på if
                 })       
             })
+            // Catch funktion som fångar upp fel om data inte hämtas korrekt:
             .catch(function(err) {
                 console.log(JSON.stringify(err));
             });
     }
 
+// Får åtkomst till paragrafer och knapp i HTML-filen:
 let besokt = document.getElementById('btn-besokt');
 let inv = document.getElementById('inv');
 let paragr = document.getElementById('para');
 
+// Skapar en tom array att lagra data i för länder som användaren besökt:
 let arrayBesokt = [];
 
+// Skapar en click funktion för "besökt" knappen:
 besokt.addEventListener('click', function() {
 
+    // Hämtar data för städer:
     fetch('stad.json')
     .then(response => response.json())
     .then(function(staderbkn) {
-
+        
+        /* Jämför om invånarantalet i pargrafen med invånarantal 
+        överrensstämmer med invånarantaler för nogot land i JSON datan:*/
         let inve = inv.innerHTML;
     
+        // Överrensstämmer invånarantalet, lagras datan i en array:
         for (s=0; s<staderbkn.length; s++) {
 
             let invj = JSON.stringify(staderbkn[s].population)
@@ -197,33 +223,46 @@ besokt.addEventListener('click', function() {
                 let value = JSON.stringify(staderbkn[s].id);
 
                 arrayBesokt.push(value);
-            
-                alert('Hurra');
+                
+                // Informerar användaren att data lagras:
+                alert('Landet du besökt sparas');
                 
             } // Slut på if
         }
         return arrayBesokt;
     })
+    // Catch funktion som fångar upp fel om data inte hämtas korrekt:
     .catch(err => console.log(JSON.stringify(err)));
     
 }) // Slut på eventlistener
 
+/* Får åtkomst till ytterligare element, och ett 
+element som man ska klicka på för att komma tille 
+en sida med lagrade städer som användaren besökt:*/
 let liBes = document.getElementById('li-Bes');
 let sect1 = document.getElementById('sect');
 
+    /* Skapar en click funtion för elementet som tar 
+    användaren till en sida med lagrade städer som användaren besökt:*/
     liBes.addEventListener('click', function() {
 
+        /* Får åtkomst till <div>, rubriker och paragrafer i HTML-filen. 
+        Dessa innehåller data för städer som användaren beökt:*/
         let dive = document.getElementById('div-E');
         let paragr = document.getElementById('p-E');
         let paragr2 = document.getElementById('p-E2');
         let btntb = document.getElementById('B-tb');
         let btnmod = document.getElementById('Rd-B');
 
+        /* Modifierar dokumentet när användaren trycker på elementet 
+        som tar användaren till en sida, som innehåller information om städer
+        användarenn besökt:*/
         dive.style.display = 'block';
 
         navb.style.display = 'none';
         sect1.style.display = 'none';
 
+        // Hämtar data för städer:
         fetch('stad.json')
             .then(function(response) {
                 return response.json();
@@ -235,6 +274,7 @@ let sect1 = document.getElementById('sect');
                 return b.population - a.population;
                 });
 
+                // Lagrar besökta städer i en array:
                 let Cities = [];
 
                 for (u=0; u<stadermod.length; u++) {
@@ -246,18 +286,20 @@ let sect1 = document.getElementById('sect');
                     } // Slut på loop
                 } // SLut på loop
 
+                // Lagrar besökta städer i localStorage:
                 let Cityn = 'Cityname';
         
                 if (Cities[0] !== undefined ) {
                     localStorage.setItem(Cityn, JSON.stringify(Cities));
                 
-                }
+                } // Slut på if
 
                 let Cities1 = localStorage.key(0);
                 let Cities2 = localStorage.getItem(Cities1);
                 let paragrx = document.getElementById('p-E');
                 paragrx.innerHTML += 'Besökt stad: ' + Cities2 + ' <br />';
-                    
+                
+                // Lagrar summan, d.v.s. det totala invånarantalet i de baesökta städerna, i en array:
                 let sum = [];
                 for (v=0; v<stadermod.length; v++) {
                     for (q=0; q<arrayBesokt.length; q++) {
@@ -278,10 +320,11 @@ let sect1 = document.getElementById('sect');
                 
                     let Sum3 = 'InvTotalt';
         
+                    // Lagrar summan av invånarantalet i localStorage:
                     if (Cities[0] !== undefined ) {
                         localStorage.setItem(Sum3, JSON.stringify(sum2));
                     
-                    }
+                    } // Slut på if
     
                     let Tot1 = localStorage.key(1);
                     let Tot2 = localStorage.getItem(Tot1);
@@ -290,6 +333,8 @@ let sect1 = document.getElementById('sect');
             
             })
 
+            /* Skapar en click funktion för knappen som tar tillbaka 
+            användaren till sidan där man kan välja städer och länder*/
             btntb.addEventListener('click', function() {
                 navb.style.display = 'block';
                 sect1.style.display = 'block';
@@ -299,6 +344,7 @@ let sect1 = document.getElementById('sect');
                 paragr2.innerHTML = '';
             })
 
+            // Skapar en click funktion som raderar historik och sparad användardata:
             btnmod.addEventListener('click', function() {
                 localStorage.clear();
                 location.reload();
